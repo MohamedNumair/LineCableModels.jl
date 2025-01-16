3421274ddcb263daf2129369c825e6e46d8c022e
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2025-01-16 09:48:02 +0100

    Add dependency installation and update Project.toml for required packages

---
+++ deps.jl
@@ -0,0 +1,12 @@
+using Pkg
+
+Pkg.activate(".")
+Pkg.add("Measurements")
+Pkg.add("CSV")
+Pkg.add("DataFrames")
+Pkg.add("SpecialFunctions")
+Pkg.add("Colors")
+Pkg.add("ColorSchemes")
+Pkg.add("Plots")
+Pkg.add("Statistics")
+Pkg.add("LinearAlgebra")
