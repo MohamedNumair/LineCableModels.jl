ca580c620d1d23c9d362236618bc86fec647413f
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2025-08-14 19:46:04 +0200

    test: reindent runtests.jl for consistency

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,17 +2,21 @@ using LineCableModels
 using Test
 
 @testset "LineCableModels.jl tests" begin
-	@info "Running BaseParams tests..."
-	@testset "BaseParams module" begin
-		include("baseparams.jl")
-	end
-	@info "BaseParams tests completed."
+    @info "Running BaseParams tests..."
+    @testset "BaseParams module" begin
+        include("baseparams.jl")
+    end
+    @info "BaseParams tests completed."
 
-	@testset "DataModel module 1/1" begin
-		include("datamodel.jl")
-	end
-	@info "DataModel tests completed."
+    @testset "DataModel module 1/1" begin
+        include("datamodel.jl")
+    end
+    @info "DataModel tests completed."
 
+    # @testset "Integration tests based on tutorial files" begin
+    #     include("tutorial1.jl")
+    # end
+    @info "Tutorials tests completed."
 
-	@info "All tests completed."
+    @info "All tests completed."
 end
