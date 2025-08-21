f54a1146191db32f00c04ed058b86f6bdc7f9427
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2025-08-22 00:33:31 +0200

    test: migrate to TestItemRunner and @testitem structure

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,31 +1,54 @@
 using LineCableModels
 using Test
+using TestItemRunner
 
-@testset "LineCableModels.jl tests" begin
-    @info "Running BaseParams tests..."
-    @testset "BaseParams module" begin
-        include("baseparams.jl")
-    end
-    @info "BaseParams tests completed."
+@testsnippet commons begin
+    const TEST_TOL = 1e-8
+    using Measurements
+    using Measurements: measurement, uncertainty, value
+    using DataFrames
 
-    @testset "DataModel module 1/1" begin
-        include("datamodel.jl")
-    end
-    @info "DataModel tests completed."
-
-    @testset "EarthProps module" begin
-        include("earthprops.jl")
-    end
-    @info "EarthProps tests completed."
-
-    @testset "Integration tests based on example files" begin
-        exa_files = ["test_tutorial1.jl", "test_tutorial2.jl", "test_tutorial3.jl"]
-        for f in exa_files
-            include(f)
-        end
-    end
-
-    @info "Tutorials tests completed."
-
-    @info "All tests completed."
 end
+
+@run_package_tests
+
+# @testset "LineCableModels.jl tests" begin
+#     @info "Running BaseParams tests..."
+#     @testitem "BaseParams module" begin
+#         include("baseparams.jl")
+#     end
+#     @info "All tests completed."
+# end
+
+# @testset "LineCableModels.jl tests" begin
+#     @info "Running BaseParams tests..."
+#     @testitem "BaseParams module" begin
+#         include("baseparams.jl")
+#         for file in sort(filter(f -> endswith(f, ".jl"), readdir("test/unit_BaseParams", join=true)))
+#             include(file)
+#         end
+#         @info "BaseParams tests completed."
+#     end
+
+
+#     @testitem "DataModel module 1/1" begin
+#         include("datamodel.jl")
+#         @info "DataModel tests completed."
+#     end
+
+#     @testitem "EarthProps module" begin
+#         include("earthprops.jl")
+#         @info "EarthProps tests completed."
+
+#     end
+
+#     @testitem "Integration tests based on example files" begin
+#         exa_files = ["test_tutorial1.jl", "test_tutorial2.jl", "test_tutorial3.jl"]
+#         for f in exa_files
+#             include(f)
+#         end
+#         @info "Tutorials tests completed."
+#     end
+
+#     @info "All tests completed."
+# end
