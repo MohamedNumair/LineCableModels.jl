e8c3b8b9dfa5f0b732554b9451fc7ed21a0f0aef
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2025-08-30 04:43:43 +0200

    chore(engine): update base.jl

--- src/engine/base.jl
+++ src/engine/base.jl
@@ -1,7 +1,6 @@
-import Base: show
 
 # Pretty printing with uncertainty information if present
-function show(io::IO, params::LineParameters{T}) where {T}
+function Base.show(io::IO, params::LineParameters{T}) where {T}
     n_cond, _, n_freq = size(params.Z)
     print(io, "LineParameters with $(n_cond) conductors at $(n_freq) frequencies")
     if T <: Complex{Measurement{Float64}}
