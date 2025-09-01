3d346f524cc3802bf00b079d50d4cb59c9da941e
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2025-09-01 21:38:05 +0200

    refactor(engine): update compute! to use new LineParameters and return ZY, ZY012

--- src/engine/solver.jl
+++ src/engine/solver.jl
@@ -1,23 +1,31 @@
-function compute!(problem::LineParametersProblem,
-    formulation::CoaxialFormulation)
+function compute!(problem::LineParametersProblem{T},
+    formulation::CoaxialFormulation) where {T}
 
     # Flatten the system into 1D arrays for computation
     workspace = init_workspace(problem, formulation)
 
-    T = eltype(workspace.freq)
     ComplexT = Complex{T}
 
+    @info "Preallocating arrays"
     n_frequencies = workspace.n_frequencies
     n_phases = workspace.n_phases
     Z = zeros(ComplexT, n_phases, n_phases, n_frequencies)
     Y = zeros(ComplexT, n_phases, n_phases, n_frequencies)
 
-    @show workspace
-    @show typeof(workspace)
-
     @info "Starting line parameters computation"
-    line_params = nothing
+
+    Z = _compute_impedance_matrix_from_ws(workspace)
+    Y = _compute_admittance_matrix_from_ws(workspace)
+
+    @info "Computing symmetrical components"
+
+    Z012 = CableToolbox.apply_fortescue_transform(Z)
+    Y012 = CableToolbox.apply_fortescue_transform(Y)
+
+    ZY = LineParameters(Z, Y)
+    ZY012 = LineParameters(Z012, Y012)
 
     @info "Line parameters computation completed successfully"
-    return workspace, line_params
+    return workspace, ZY, ZY012
 end
+
