389b32b4188173574d49f0237b0e7ac50eaa1873
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2025-09-04 02:06:37 +0200

    feat(internalimpedance): add loop_to_phase transformation for impedance matrices

--- src/engine/InternalImpedance.jl
+++ src/engine/InternalImpedance.jl
@@ -19,9 +19,36 @@ using ...Commons
 import ...Commons: get_description
 import ..Engine: InternalImpedanceFormulation
 using Measurements
+using LinearAlgebra
 
 struct ScaledBessel <: InternalImpedanceFormulation end
 get_description(::ScaledBessel) = "Scaled Bessel (Schelkunoff)"
+
+
+
+
+
+function loop_to_phase(
+    Z::Matrix{Complex{T}},
+) where {T<:REALSCALAR}
+    # Check the size of the Z matrix (assuming Z is NxN)
+    N = size(Z, 1)
+
+    # Build the voltage transformation matrix T_V
+    T_V = Matrix{T}(I, N, N + 1)  # Start with an identity matrix
+    for i ∈ 1:N
+        T_V[i, i+1] = -1  # Set the -1 in the next column
+    end
+    T_V = T_V[:, 1:N]  # Remove the last column
+
+    # Build the current transformation matrix T_I
+    T_I = tril(ones(T, N, N))  # Lower triangular matrix of ones
+
+    # Compute the new impedance matrix Z_prime
+    Z_prime = T_V \ Z * T_I
+
+    return Z_prime
+end
 
 
 macro uncertain_bessel(expr::Expr)
