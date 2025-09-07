97e35421f215b3a301ac43f4468fd1e368e0844b
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2025-09-07 23:35:52 +0200

    refactor(insulationimpedance): rename PureInductance to Lossless and update description

--- src/engine/insulationimpedance/InsulationImpedance.jl
+++ src/engine/insulationimpedance/InsulationImpedance.jl
@@ -21,30 +21,30 @@ import ...Commons: get_description
 import ..Engine: InsulationImpedanceFormulation
 using Measurements
 
-struct PureInductance <: InsulationImpedanceFormulation end
-get_description(::PureInductance) = "Pure inductance (lossless dielectric)"
+struct Lossless <: InsulationImpedanceFormulation end
+get_description(::Lossless) = "Lossless insulation (ideal dielectric)"
 
 function calc_outer_insulation_impedance(
-    radius_ex::Measurement{T},
-    radius_in::Measurement{T},
-    mur_ins::Measurement{T},
-    f::T,
-) where {T<:Real}
+	radius_ex::Measurement{T},
+	radius_in::Measurement{T},
+	mur_ins::Measurement{T},
+	f::T,
+) where {T <: Real}
 
-    # Constants
-    m0 = 4 * pi * 1e-7
-    mu_ins = m0 * mur_ins
-    omega = 2 * pi * f
+	# Constants
+	m0 = 4 * pi * 1e-7
+	mu_ins = m0 * mur_ins
+	omega = 2 * pi * f
 
-    # Avoid division by zero if radius_in is 0
-    if radius_in == 0
-        radius_in = eps()
-    end
+	# Avoid division by zero if radius_in is 0
+	if radius_in == 0
+		radius_in = eps()
+	end
 
-    # Impedance of the insulation layer
-    zinsu = im * omega * mu_ins * log(radius_ex / radius_in) / (2 * pi)
+	# Impedance of the insulation layer
+	zinsu = im * omega * mu_ins * log(radius_ex / radius_in) / (2 * pi)
 
-    return zinsu
+	return zinsu
 end
 
 end # module InsulationImpedance
