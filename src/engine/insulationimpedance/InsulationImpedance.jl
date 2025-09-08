d4e54463f26433c9314fa44badc1d8b2f6e40d53
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2025-09-08 23:48:55 +0200

    refactor(insulationimpedance): move Lossless type to lossless.jl and update interface

--- src/engine/insulationimpedance/InsulationImpedance.jl
+++ src/engine/insulationimpedance/InsulationImpedance.jl
@@ -12,7 +12,7 @@ $(EXPORTS)
 module InsulationImpedance
 
 # Export public API
-export PureInductance
+export Lossless
 
 
 # Module-specific dependencies
@@ -21,30 +21,6 @@ import ...Commons: get_description
 import ..Engine: InsulationImpedanceFormulation
 using Measurements
 
-struct Lossless <: InsulationImpedanceFormulation end
-get_description(::Lossless) = "Lossless insulation (ideal dielectric)"
-
-function calc_outer_insulation_impedance(
-	radius_ex::Measurement{T},
-	radius_in::Measurement{T},
-	mur_ins::Measurement{T},
-	f::T,
-) where {T <: Real}
-
-	# Constants
-	m0 = 4 * pi * 1e-7
-	mu_ins = m0 * mur_ins
-	omega = 2 * pi * f
-
-	# Avoid division by zero if radius_in is 0
-	if radius_in == 0
-		radius_in = eps()
-	end
-
-	# Impedance of the insulation layer
-	zinsu = im * omega * mu_ins * log(radius_ex / radius_in) / (2 * pi)
-
-	return zinsu
-end
+include("lossless.jl")
 
 end # module InsulationImpedance
