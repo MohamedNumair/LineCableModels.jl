0d273f2d14b910599fa26ab94c9e5304c9b230dd
amaurigmartins <amauri.martinsbritto@kuleuven.be>
2025-09-26 22:15:25 +0200

    feat(transforms): import NLsolve and include eiglevenberg.jl

--- src/engine/transforms/Transforms.jl
+++ src/engine/transforms/Transforms.jl
@@ -17,13 +17,17 @@ export Fortescue
 # Module-specific dependencies
 using ...Commons
 import ...Commons: get_description
-import ...Utils: symtrans, offdiag_ratio
-import ..Engine: AbstractTransformFormulation, LineParameters
+import ...Utils: symtrans, offdiag_ratio, to_nominal
+import ..Engine:
+	AbstractTransformFormulation, LineParameters, SeriesImpedance, ShuntAdmittance
 using Measurements
 using LinearAlgebra
+# using GenericLinearAlgebra
+using NLsolve
 
 
 include("fortescue.jl")
+include("eiglevenberg.jl")
 
 
 end # module Transforms
